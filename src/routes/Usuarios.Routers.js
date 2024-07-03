4b0996c1f63c7ce449450ece58579b9fd0260f63
jmsocorro <jsocorro@sismatica.net>
2024-07-03 07:36:09 -0300

    usuarios find by UUID update UsuariosRouters

--- src/routes/Usuarios.Routers.js
+++ src/routes/Usuarios.Routers.js
@@ -3,7 +3,7 @@ import { UsuarioController } from "../controllers/Usuarios.Controller.js";
 
 const router = Router();
 router.get("/", UsuarioController.getAll);
-router.get("/uuid", UsuarioController.findbyuuid);
+router.get("/uuid/:uuid", UsuarioController.findbyuuid);
 router.post("/nuevo", UsuarioController.addOne);
 router.put("/", UsuarioController.updateOne);
 router.delete("/", UsuarioController.deleteOne)
